b08c9ccb1ba398ee26fdd3ffa77f560cd680ed88
Don Carnohan <donaldcarnohan@gmail.com>
2019-04-18 00:50:54 -0400

    Start organizing the code, created ball module

--- lib/demo_web/live/pong/pong.ex
+++ lib/demo_web/live/pong/pong.ex
@@ -1,25 +1,17 @@
 defmodule DemoWeb.Pong do
   use Phoenix.LiveView
-
+  alias DemoWeb.Pong.Ball
   @tick 20
-  @ball_size 16
   @paddle_size 100
   
-  #Speed is measured in pixels per tick
-  @ball_speed %{x: 2, y: -20}
-
-  @ball_position %{x: 200, y: 200}
   @player_1_position 200
 
   @width 400
   @height 400
   @paddle_speed 10
   @paddle_offset 50
-
-  defmacro x_min(), do: @ball_size/2
-  defmacro x_max(), do: @width - @ball_size/2
-  defmacro y_min(), do: @paddle_offset + @ball_size/2
-  defmacro y_max(), do: @height - @paddle_offset - @ball_size/2
+  @ball_size 15
+  
 
   def render(%{game_state: :over} = assigns) do
     ~L"""
@@ -42,8 +34,8 @@ defmodule DemoWeb.Pong do
           <option value="20" <%= if @tick == 200, do: "selected" %>>200</option>
           <option value="50" <%= if @tick == 500, do: "selected" %>>500</option>
         </select>
-        <input type="range" min="5" max="50" name="width" value="<%= @ball_size %>" />
-        <%= @ball_size %>px
+        <input type="range" min="5" max="50" name="width" value="<%= @ball.size %>" />
+        <%= @ball.size %>px
       </form>
     </div>
     <h3 class="score">SCORE:&nbsp;<%= @score %></h3>
@@ -63,10 +55,10 @@ defmodule DemoWeb.Pong do
         class="ball"
         style="
           position:absolute;
-          left: <%= @ball_position.x - @ball_size/2 %>px;
-          bottom: <%= @ball_position.y - @ball_size/2 %>px;
-          width: <%= @ball_size %>px;
-          height: <%= @ball_size %>px;
+          left: <%= @ball.position.x - @ball.size/2 %>px;
+          bottom: <%= @ball.position.y - @ball.size/2 %>px;
+          width: <%= @ball.size %>px;
+          height: <%= @ball.size %>px;
           background-color: white;
         "
       ></div>
@@ -106,28 +98,24 @@ defmodule DemoWeb.Pong do
       score: 0,
       game_state: :playing,
       heading: :stationary,
-      pending_headings: %{left: False, right: False},
-      ball_size: @ball_size,
-      ball_speed: @ball_speed,
+      pending_headings: %{left: false, right: false},
+      ball: %Ball{
+        size: @ball_size,
+        bounds: %{
+          x: %{min: @ball_size/2, max: @width - @ball_size/2},
+          y: %{min: @paddle_offset + @ball_size/2, max: @height - @paddle_offset - @ball_size/2},
+        },
+      },
       paddle_size: @paddle_size,
       paddle_offset: @paddle_offset,
       width: @width,
       height: @height,
       tick: @tick,
-      ball_position: @ball_position,
       player_1_position: @player_1_position,
     }
-
-    new_socket =
-      socket
-      |> assign(defaults)
-
-    if connected?(new_socket) do
-      #some setup
-      new_socket
-    else
-      new_socket
-    end
+et
+    |> 
+    sockassign(defaults)
   end
 
   def handle_event("update_settings", %{"width" => width, "tick" => tick}, socket) do
@@ -191,17 +179,17 @@ defmodule DemoWeb.Pong do
   defp unset_heading(socket, :none), do: socket
 
   defp set_heading(socket, heading) do
-    update(socket, :pending_headings, &(Map.put(&1, heading, True)))
+    update(socket, :pending_headings, &(Map.put(&1, heading, true)))
   end
   defp unset_heading(socket, heading) do
-    update(socket, :pending_headings, &(Map.put(&1, heading, False)))
+    update(socket, :pending_headings, &(Map.put(&1, heading, false)))
   end
 
   defp next_heading(socket) do
     case socket.assigns.pending_headings do
       %{left: left, right: right} when left == right -> :stationary
-      %{left: True, right: False} -> :left
-      %{left: False, right: True} -> :right
+      %{left: true, right: false} -> :left
+      %{left: false, right: true} -> :right
     end
   end
 
@@ -213,98 +201,36 @@ defmodule DemoWeb.Pong do
     end
   end
 
-  defp bounce_ball(socket) do
-  end
-
-  defp get_ball_intersect_bottom(socket) do
-    x = socket.assigns.ball_position.x
-    y = socket.assigns.ball_position.y
-    slope_x = socket.assigns.ball_speed.x
-    slope_y = socket.assigns.ball_speed.y
-    slope = slope_y / slope_x
-    offset = y - slope * x
-    (y_min - offset) / slope
-  end
-
-  defp get_ball_intersect_top(socket) do
-    x = socket.assigns.ball_position.x
-    y = socket.assigns.ball_position.y
-    slope_x = socket.assigns.ball_speed.x
-    slope_y = socket.assigns.ball_speed.y
-    slope = slope_y / slope_x
-    offset = y - slope * x
-    (y_max - offset) / slope
-  end
-
-
-  defp next_ball_position(socket) do
-    ball_speed = socket.assigns.ball_speed
-    {ball_speed_x, next_x_position} = case socket.assigns.ball_position.x + ball_speed.x do
-        position when position < x_min or position > x_max ->
-          overshoot = cond do
-            position < x_min -> position - x_min
-            position > x_max -> x_max - position
-          end
-          {-ball_speed.x, socket.assigns.ball_position.x - ball_speed.x + overshoot}
-        position ->
-          {socket.assigns.ball_speed.x, position}
-    end
-    next_x_position = socket.assigns.ball_position.x + socket.assigns.ball_speed.x
-    {gameover, ball_speed_y, next_y_position, points} = case socket.assigns.ball_position.y + ball_speed.y do
-        position when position < y_min or position > y_max ->
-          {x_intercept, overshoot} = cond do
-            position < y_min -> {get_ball_intersect_bottom(socket), position - y_min}
-            position > y_max -> {get_ball_intersect_top(socket), y_max - position}
-          end
-          if abs(x_intercept - socket.assigns.player_1_position) > @paddle_size/2 + @ball_size/2 do
-            {true, ball_speed.y, position, 0}
-          else
-            {false, -ball_speed.y, socket.assigns.ball_position.y - ball_speed.y + overshoot, 1}
-          end
-        position -> {false, socket.assigns.ball_speed.y, position, 0}
-    end
-    IO.puts(gameover)
-    if gameover do
-      game_over(socket)
-    else
-      socket
-      |> update(:ball_position, fn _ -> %{x: next_x_position, y: next_y_position} end)
-      |> update(:ball_speed, fn _ -> %{x: ball_speed_x, y: ball_speed_y} end)
-      |> update(:score, fn s -> s + points end)
-    end
-  end
 
   # defp game_loop(%{assigns: %{pending_headings: %{}} = socket), do: socket
+
+  defp paddle_hit_ball?(paddle_position, ball) do
+    distance_to_ball = abs(paddle_position - Ball.get_intersect(ball))
+    distance_to_ball < @paddle_size/2 + ball.size/2
+  end
+
 
   defp game_loop(socket) do
     if socket.assigns.game_state == :playing do
       heading = next_heading(socket)
-      # {row_before, col_before} = coord(socket)
-      # maybe_row = row(row_before, heading)
-      # maybe_col = col(col_before, heading)
       paddle_position = next_paddle_position(heading, socket)
 
+      if Ball.will_go_out_of_bounds?(socket.assigns.ball, :y)
+      and not paddle_hit_ball?(paddle_position, socket.assigns.ball) do
+        socket
+        |> game_over()
+      else
+        socket
+        |> update(:ball, fn ball ->  Ball.next_position(ball) end)
+        |> update(:heading, fn _ -> heading end)
+        |> update(:player_1_position, fn _ -> paddle_position end)
+      end
 
-      socket
-      |> next_ball_position()
-      |> update(:heading, fn _ -> heading end)
-      |> update(:player_1_position, fn _ -> paddle_position end)
     else
       socket
     end
   end
 
-
-  # def handle_collision(socket, :tail), do: game_over(socket)
-  #def handle_collision(socket, :wall), do: wall_bounce(socket)
-  # def handle_collision(socket, :paddle), do: paddle_bounce(socket)
-  # def handle_collision(socket, :empty), do: socket
-
   defp game_over(socket), do: assign(socket, :game_state, :over)
 
-  # defp paddle_bounce(socket) do
-  #   socket
-  #   |> assign(:score, socket.assigns.score + 1)
-  #   |> assign(:ball_speed, %{x: @ball_speed.x, y: -@ball_speed.y})
-  # end
 end
